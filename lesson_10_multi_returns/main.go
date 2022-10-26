7ee38e8463da83123f9484cb0357b25f675c372c
Alex <alja.platt@gmail.com>
2022-10-26 10:09:20 +0100

    package scope

---
+++ lesson_10_multi_returns/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//? if we want to return 2 values you can put the values in brackets and comma separate them (string, string)
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+}
+
+func main() {
+	fn1, sn1 := getInitials("tifa lockhart")
+	fmt.Println(fn1, sn1)
+
+	fn2, sn2 := getInitials("cloud strife")
+	fmt.Println(fn2, sn2)
+
+	fn3, sn3 := getInitials("barret")
+	fmt.Println(fn3, sn3)
+}
